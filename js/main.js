cafe2d33d3f4570a1258baababb9d6bc854597aa
Giovanna Ruiu <giovanna.ruiu@analogfolk.com>
2015-03-09 17:49:04 +0000

    General styling

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ var application = (function() {
 
 	function addListItem() {
 		$('.list').click(function(){
-		    var text = $('#listName').val() + '<button>x</button>';
+		    var text = $('#listName').val() + '<button class="fa fa-times"></button>';
 		    if(text.length){
-		        $('<li />', {html: text}).appendTo('ul.justList')
+		        $('<li />', {html: "£ " + text}).appendTo('ul.justList');
 		    }
 		});
 
 		$('ul').on('click','button' , function(el){
-		    $(this).parent().remove()
+		    $(this).parent().remove();
 		});
 	}
 
