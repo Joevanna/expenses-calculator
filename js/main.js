909298be35da9e598c361c793691f1106bc20eb8
Giovanna Ruiu <giovanna.ruiu@analogfolk.com>
2015-03-11 17:51:29 +0000

    First result functionality added

--- js/main.js
+++ js/main.js
@@ -1,21 +1,44 @@
 var application = (function() {
 
 	function addListItem() {
-		$('.list').click(function(){
-		    var text = $('#listName').val() + '<button class="fa fa-times"></button>';
-		    if(text.length){
-		        $('<li />', {html: "£ " + text}).appendTo('ul.justList');
-		    }
+	    var text = $('#listName').val() + '<button class="fa fa-times"></button>';
+	    if(text.length){
+	        $('<li />', {html: text}).appendTo('ul.justList');
+	    }
+	}
+
+	function removeListItem() {
+		$(this).parent().remove();
+	}
+
+	function expenses() {
+		var sum = 0;
+		$('.justList').find('li').each(function() {
+			sum += $(this).text();
+			console.log($(this));
 		});
 
-		$('ul').on('click','button' , function(el){
-		    $(this).parent().remove();
-		});
+
+		result(sum)
+	}
+
+
+	function result(sum) {
+
+		$('.flipper').addClass('turn');
+		$('.figure').html($('.initialSum').val() - sum);
+
+	}
+
+
+	function events() {
+		$('.list').on('click', addListItem);
+		$('ul').on('click','button', removeListItem);
+		$('.trigger').on('click', expenses);
 	}
 
 	function init() {
-		addListItem();
-
+		events();
 	}
 
 	return {
