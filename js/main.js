92247891e5f00ad0a7bd29d1a984435fdd23c960
Giovanna Ruiu <giovanna.ruiu@analogfolk.com>
2015-03-27 16:49:56 +0000

    Functionality and styling

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 var application = (function() {
 
+	function changeClass() {
+		$('.buttons button').removeClass('active');
+		$(this).addClass('active');
+	}
+
 	function addListItem() {
 	    var text = $('#listName').val() + '<button class="fa fa-times"></button>';
 	    if(text.length){
 	        $('<li />', {html: text}).appendTo('ul.justList');
+	        $('#listName').val('');
 	    }
 	}
 
@@ -13,28 +19,51 @@ var application = (function() {
 
 	function expenses() {
 		var sum = 0;
+
 		$('.justList').find('li').each(function() {
-			sum += $(this).text();
-			console.log($(this));
+			sum += parseInt($(this).text(), 10);
+			
 		});
+
+		
 
 
 		result(sum)
 	}
 
-
 	function result(sum) {
 
-		$('.flipper').addClass('turn');
-		$('.figure').html($('.initialSum').val() - sum);
+		var initialSum = $('.initialSum').val(),
+			finalTot = initialSum - sum,
+			percentage = (finalTot / 100) * 10;
 
+		setTimeout(function() {
+			$('.flipper').addClass('turn');
+		}, 1000);
+		
+		if ($('.daily').hasClass('active')) {
+
+			$('.figure').html(percentage.toFixed(2) / 30);
+			$('.your-result p').first().show();
+
+		} else if ($('.weekly').hasClass('active')) {
+
+			$('.figure').html(percentage.toFixed(2) / 4);
+			$('.your-result p:nth-child(odd)').show();
+
+		} else if ($('.monthly').hasClass('active')) {
+
+			$('.figure').html(percentage.toFixed(2));
+			$('.your-result p').last().show();
+
+		} 			
 	}
-
 
 	function events() {
 		$('.list').on('click', addListItem);
 		$('ul').on('click','button', removeListItem);
 		$('.trigger').on('click', expenses);
+		$('.buttons button').on('click', changeClass);
 	}
 
 	function init() {
